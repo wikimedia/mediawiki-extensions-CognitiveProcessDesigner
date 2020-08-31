2c1aae95bde7b282c82dfe3ee03d593e7da84e02
Dejan Savuljesku <d.savuljesku@gmail.com>
2020-08-31 09:49:21 +0200

    Do no use window.onload
    [all]
    
    ERM 20588
    
    Change-Id: Ibdfe44b66ec0fb66c27d5f1367116937bb610324
    (cherry picked from commit f3fb55cca2e4cdace7ec4f35554aea971f5c424b)

--- resources/js/cognitiveprocessdesigner.special.edit.js
+++ resources/js/cognitiveprocessdesigner.special.edit.js
@@ -1,13 +1,16 @@
-window.onload = function() {
-	var showPrompt = function() {
-		OO.ui.prompt( mw.msg('cpd-enter-bpmn-id-placeholder') )
-			.done( function ( bpmnName ) {
-				mw.cpdManager.specialPageMode = false;
-				mw.cpdManager.cancelCallback = showPrompt;
-				mw.cpdManager.editBPMN(
-					bpmnName, $( '#cpd-wrapper'  ), $( '#cpd-img' ), $( '#cpd-btn-edit-bpmn-id' )
-				);
-			});
-	};
-	showPrompt();
-};
+( function( $ ) {
+	$( function() {
+		var showPrompt = function() {
+			OO.ui.prompt( mw.msg('cpd-enter-bpmn-id-placeholder') )
+				.done( function ( bpmnName ) {
+					mw.cpdManager.specialPageMode = false;
+					mw.cpdManager.cancelCallback = showPrompt;
+					mw.cpdManager.editBPMN(
+						bpmnName, $( '#cpd-wrapper'  ), $( '#cpd-img' ), $( '#cpd-btn-edit-bpmn-id' )
+					);
+				});
+		};
+		showPrompt();
+	} );
+} )( jQuery );
+
