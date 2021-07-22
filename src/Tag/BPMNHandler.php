bf77756dba96ff9f256e3cee1cb4a27829b8d177
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2021-07-22 06:16:00 +0000

    build: Updating browserslist to 4.16.6
    * https://npmjs.com/advisories/1747 (CVE-2021-23364)
    
    Change-Id: Ie54b729ef87884d8ddb7d423b01f1b584ffb9a8b

--- src/Tag/BPMNHandler.php
+++ src/Tag/BPMNHandler.php
@@ -67,7 +67,7 @@ class BPMNHandler {
 	 */
 	public function handle() {
 		if ( !isset( $this->tagArgs['name'] ) || $this->tagArgs['name'] === '' ) {
-			return Html::errorBox('"name" attribute of diagram must be specified!');
+			return Html::errorBox( '"name" attribute of diagram must be specified!' );
 		}
 
 		$bpmnName = wfStripIllegalFilenameChars( $this->tagArgs['name'] );
