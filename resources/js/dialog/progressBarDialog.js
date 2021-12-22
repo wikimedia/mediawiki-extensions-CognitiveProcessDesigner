349424de9969cf602cbfb7e71129fb8174505728
akulbii <alk@astarios.com>
2021-12-22 06:12:45 +0200

    Refactor diagram saving API calls
    Because of many API requests during diagram saving, ratelimit problem occured.
    It was decided to refactor diagram saving to do most of the work on the backend.
    So there will be only few API calls instead of dozens.
    
    [3.2.x]
    
    ERM26940
    
    Change-Id: I1db53a902e05b1ff2d42cd6ea182fddc51354ead

---
+++ resources/js/dialog/progressBarDialog.js
@@ -0,0 +1,35 @@
+bs.util.registerNamespace( 'ext.cpd' );
+
+ext.cpd.ProgressBarDialog = function( config ) {
+    ext.cpd.ProgressBarDialog.super.call( this, config );
+};
+OO.inheritClass( ext.cpd.ProgressBarDialog, OO.ui.Dialog );
+
+ext.cpd.ProgressBarDialog.static.name = 'cpd-diagram-save-dialog';
+ext.cpd.ProgressBarDialog.static.title = mw.message( 'cpd-diagram-save-dialog-title' ).text();
+
+ext.cpd.ProgressBarDialog.prototype.setup = function( data ) {
+	this.content = new OO.ui.PanelLayout( {
+		padded: true,
+		expanded: false
+	} );
+
+	var progressBar = new OO.ui.ProgressBarWidget();
+
+	var elementsAmount = data.elementsAmount;
+
+	var progressField = new OO.ui.FieldLayout( progressBar, {
+		label: mw.message( 'cpd-diagram-save-dialog-span', elementsAmount ).text(),
+		align: 'top'
+	} );
+
+	this.content.$element.append( progressField.$element );
+
+	this.$body.append( this.content.$element );
+
+	return ext.cpd.ProgressBarDialog.super.prototype.setup.call( this, data );
+};
+
+ext.cpd.ProgressBarDialog.prototype.getBodyHeight = function () {
+	return this.content.$element.outerHeight( true );
+};
---
+++ src/Api/SaveDiagramElementsApi.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace CognitiveProcessDesigner\Api;
+
+use ApiBase;
+use CommentStoreComment;
+use ContentHandler;
+use MediaWiki\Revision\SlotRecord;
+use Message;
+use MWException;
+use RuntimeException;
+use TextContent;
+use Title;
+use Wikimedia\ParamValidator\ParamValidator;
+use WikiPage;
+
+class SaveDiagramElementsApi extends ApiBase {
+
+	/**
+	 * @inheritDoc
+	 */
+	protected function getAllowedParams() {
+		return [
+			'elements' => [
+				ParamValidator::PARAM_TYPE => 'string',
+				ParamValidator::PARAM_REQUIRED => true,
+				ApiBase::PARAM_HELP_MSG => 'cpd-api-save-diagram-elements-param-elements'
+			]
+		];
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function needsToken() {
+		return 'csrf';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function execute() {
+		$params = $this->extractRequestParams();
+
+		$elements = json_decode( $params['elements'], true );
+
+		$errors = [];
+		$warnings = [];
+
+		if ( $elements ) {
+			foreach ( $elements as $element ) {
+				$new = false;
+
+				$title = Title::makeTitle( NS_MAIN, $element['title'] );
+
+				$wikipage = WikiPage::factory( $title );
+
+				$updater = $wikipage->newPageUpdater( $this->getContext()->getUser() );
+				if ( $wikipage->exists() ) {
+					$parentRevision = $updater->grabParentRevision();
+					$content = $parentRevision->getContent( SlotRecord::MAIN );
+					if ( $content instanceof TextContent ) {
+						$text = $content->getText();
+
+						$text = preg_replace( '/<div class="cdp-data".*?<\/div>/s', '', $text );
+
+						$text = '<div class="cdp-data">' . $element['content'] . '</div>' . "\n" . $text;
+
+						$content = ContentHandler::makeContent( $text, $title );
+					}
+				} else {
+					$new = true;
+
+					$content = ContentHandler::makeContent( $element['content'], $title );
+				}
+
+				$updater->setContent( SlotRecord::MAIN, $content );
+
+				$comment = Message::newFromKey( 'cpd-api-save-diagram-elements-update-comment' );
+				$commentStore = CommentStoreComment::newUnsavedComment( $comment );
+
+				$flag = ( $new ? EDIT_NEW : EDIT_UPDATE );
+				try {
+					$result = $updater->saveRevision( $commentStore, $flag );
+				} catch ( MWException | RuntimeException $e ) {
+					$errors[$element['title']] = $e->getMessage();
+
+					continue;
+				}
+
+				if ( $result === null || !$updater->wasSuccessful() ) {
+					$status = $updater->getStatus();
+
+					if ( $status->getErrors() ) {
+						// If status is okay but there are errors - they are not fatal, just warnings
+						if ( $status->isOK() ) {
+							$warnings[$element['title']] = $status->getMessage();
+						} else {
+							$errors[$element['title']] = $status->getMessage();
+						}
+					}
+				}
+			}
+		}
+
+		$success = true;
+		if ( $errors ) {
+			$success = false;
+		}
+
+		if ( $errors ) {
+			$this->getResult()->addValue( null, 'errors', $errors );
+		}
+		if ( $warnings ) {
+			$this->getResult()->addValue( null, 'warnings', $warnings );
+		}
+
+		$this->getResult()->addValue( null, 'success', $success );
+	}
+
+}
