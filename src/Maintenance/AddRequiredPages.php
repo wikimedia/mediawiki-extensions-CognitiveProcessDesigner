3f59376eb96824cf77dfcb0e7d619dbb59584389
Hamish Slater <hamishslater@hotmail.com>
2022-06-30 10:35:46 +0200

    Fix deprecated call of 'WikiPage::doEditContent'
    ERM28911
    
    [4.x]
    
    Change-Id: I0ec5b8a9ff7c276b05d6b6fa63add8a39546e2f7

--- src/Maintenance/AddRequiredPages.php
+++ src/Maintenance/AddRequiredPages.php
@@ -2,9 +2,13 @@
 
 namespace CognitiveProcessDesigner\Maintenance;
 
+use CommentStoreComment;
+use Exception;
 use LoggedUpdateMaintenance;
 use MediaWiki\MediaWikiServices;
+use MediaWiki\Revision\SlotRecord;
 use Title;
+use User;
 use WikiPage;
 
 class AddRequiredPages extends LoggedUpdateMaintenance {
@@ -71,6 +75,8 @@ HERE
 		} else {
 			$wikiPageFactory = null;
 		}
+		$user = User::newSystemUser( 'MediaWiki default' );
+		$summary = 'Createy by Cognitive Process Designer';
 		foreach ( $this->pages as $pagename => $wikitextContent ) {
 			$title = Title::newFromText( $pagename );
 			if ( $wikiPageFactory !== null ) {
@@ -81,14 +87,20 @@ HERE
 			}
 			if ( !$wikiPage->exists() ) {
 				$this->output( "Creating page '{$title->getPrefixedDBkey()}'... " );
+				$updater = $wikiPage->newPageUpdater( $user );
 				$content = $wikiPage->getContentHandler()->makeContent( $wikitextContent, $title );
-				$summary = 'Createy by Cognitive Process Designer';
-				$status = $wikiPage->doEditContent( $content, $summary );
-				$statusText = $status->isOK() ? 'DONE' : 'FAILED';
+				$updater->setContent( SlotRecord::MAIN, $content );
+				$comment = CommentStoreComment::newUnsavedComment( $summary );
+				try {
+					$updater->saveRevision( $comment );
+				} catch ( Exception $e ) {
+					$this->error( "EXCEPTION:\n" . $e->getMessage() );
+					return false;
+				}
+				$statusText = $updater->wasSuccessful() ? 'DONE' : 'FAILED';
 				$this->output( "$statusText\n" );
 			}
 		}
-
 		return true;
 	}
 
