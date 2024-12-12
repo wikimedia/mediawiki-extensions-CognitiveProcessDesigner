0c318df745359777faf147143d27d2d3abce29f3
Daniel Riedmüller <daniel.riedmueller@hallowelt.com>
2024-12-12 14:50:18 +0100

    Fix sql create tables
    ERM39888

--- db/cpd_element_connections.sql
+++ db/cpd_element_connections.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/cpd_element_connections(
-    `from_page` VARBINARY(255) NOT NULL,
-    `to_page` VARBINARY(255) NOT NULL,
-    `process` VARBINARY(255) NOT NULL
+    `from_page` VARCHAR(255) NOT NULL,
+    `to_page` VARCHAR(255) NOT NULL,
+    `process` VARCHAR(255) NOT NULL
 ) /*$wgDBTableOptions*/;
