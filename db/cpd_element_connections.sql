007837798f616e1c1edc970434f837d987409ad9
Daniel Riedmueller <daniel.riedmueller@hallowelt.com>
2025-01-03 14:18:12 +0100

    apply schema changes
    ERM39888
    
    [5.0.x]

--- db/cpd_element_connections.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/cpd_element_connections(
-    `from_page` VARCHAR(255) NOT NULL,
-    `to_page` VARCHAR(255) NOT NULL,
-    `process` VARCHAR(255) NOT NULL,
-    `from_type` VARCHAR(255) NOT NULL,
-    `to_type` VARCHAR(255) NOT NULL
-) /*$wgDBTableOptions*/;
