8e94411cec495defb87d3a056dfe67946134126c
Daniel Riedmüller <daniel.riedmueller@hallowelt.com>
2025-04-15 14:08:36 +0200

    Remove reader limit in process overview
    ERM41670
    [5.1.x]
    
    Change-Id: If9f9543da8171ec112cdedc1327e2d647be57d2f
    (cherry picked from commit 8063de379bdcf5d4008dd58fb106b65a8994b02d)

--- src/Data/Processes/Reader.php
+++ src/Data/Processes/Reader.php
@@ -3,6 +3,8 @@
 namespace CognitiveProcessDesigner\Data\Processes;
 
 use CognitiveProcessDesigner\Util\CpdDiagramPageUtil;
+use MWStake\MediaWiki\Component\DataStore\ReaderParams;
+use MWStake\MediaWiki\Component\DataStore\ResultSet;
 use Wikimedia\Rdbms\ILoadBalancer;
 
 class Reader extends \MWStake\MediaWiki\Component\DataStore\Reader {
@@ -16,6 +18,20 @@ class Reader extends \MWStake\MediaWiki\Component\DataStore\Reader {
 		private readonly CpdDiagramPageUtil $util,
 	) {
 		parent::__construct();
+	}
+
+	/**
+	 * @param ReaderParams $params
+	 *
+	 * @return ResultSet
+	 */
+	public function read( $params ) {
+		return parent::read( new ReaderParams( [
+			ReaderParams::PARAM_QUERY => $params->getQuery(),
+			ReaderParams::PARAM_LIMIT => ReaderParams::LIMIT_INFINITE,
+			ReaderParams::PARAM_FILTER => $params->getFilter(),
+			ReaderParams::PARAM_SORT => $params->getSort()
+		] ) );
 	}
 
 	/**
