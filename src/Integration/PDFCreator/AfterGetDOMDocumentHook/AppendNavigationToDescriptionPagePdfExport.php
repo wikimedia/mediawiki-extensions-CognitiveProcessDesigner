cbb0012e963c7fdf24b86c60d8e3ce0d3820aaf1
driedmueller <daniel.riedmueller@hallowelt.com>
2025-07-30 12:11:04 +0200

    Include navigation pdf export
     ERM40389
     [master, REL1_43, REL1_43-4.1.x]
    
    Change-Id: I4a9a4eb0910cb46361fa77457ca5cd38a24e0419

---
+++ src/Integration/PDFCreator/AfterGetDOMDocumentHook/AppendNavigationToDescriptionPagePdfExport.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace CognitiveProcessDesigner\Integration\PDFCreator\AfterGetDOMDocumentHook;
+
+use CognitiveProcessDesigner\Exceptions\CpdCreateElementException;
+use CognitiveProcessDesigner\Exceptions\CpdInvalidArgumentException;
+use CognitiveProcessDesigner\Exceptions\CpdInvalidContentException;
+use CognitiveProcessDesigner\Exceptions\CpdInvalidNamespaceException;
+use CognitiveProcessDesigner\Exceptions\CpdXmlProcessingException;
+use CognitiveProcessDesigner\Util\CpdDescriptionPageUtil;
+use CognitiveProcessDesigner\Util\CpdElementConnectionUtil;
+use DOMDocument;
+use DOMException;
+use MediaWiki\Extension\PDFCreator\IPreProcessor;
+use MediaWiki\Extension\PDFCreator\Utility\ExportContext;
+use MediaWiki\Extension\PDFCreator\Utility\PageContext;
+
+class AppendNavigationToDescriptionPagePdfExport implements IPreProcessor {
+
+	/**
+	 * @param CpdDescriptionPageUtil $descriptionPageUtil
+	 * @param CpdElementConnectionUtil $connectionUtil
+	 */
+	public function __construct(
+		private readonly CpdDescriptionPageUtil $descriptionPageUtil,
+		private readonly CpdElementConnectionUtil $connectionUtil
+	) {
+	}
+
+	/**
+	 * Insert navigation
+	 *
+	 * @param DOMDocument $dom
+	 * @param PageContext $context
+	 *
+	 * @return void
+	 * @throws CpdCreateElementException
+	 * @throws CpdInvalidArgumentException
+	 * @throws CpdInvalidContentException
+	 * @throws CpdInvalidNamespaceException
+	 * @throws CpdXmlProcessingException
+	 * @throws DOMException
+	 */
+	public function onPDFCreatorAfterGetDOMDocument( DOMDocument $dom, PageContext $context ): void {
+		if ( !$this->descriptionPageUtil->isDescriptionPage( $context->getTitle() ) ) {
+			return;
+		}
+
+		$revId = $context->getRequest()->getValues( 'oldId' );
+
+		$navigationHtml = $this->connectionUtil->createNavigationHtml(
+			$context->getTitle(),
+			$revId ? (int)$revId : null
+		);
+
+		$body = $dom->getElementsByTagName( 'body' )->item( 0 );
+
+		$fragment = $dom->createDocumentFragment();
+		$fragment->appendXML( $navigationHtml );
+
+		// Create the wrapper div
+		$wrapperDiv = $dom->createElement( 'div' );
+		$wrapperDiv->appendChild( $fragment );
+
+		// Move all existing body children into the wrapper div
+		while ( $body->firstChild ) {
+			$wrapperDiv->appendChild( $body->firstChild );
+		}
+
+		$body->appendChild( $wrapperDiv );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function execute(
+		array &$pages,
+		array &$images,
+		array &$attachments,
+		ExportContext $context,
+		string $module = '',
+		$params = []
+	): void {
+		$imagesPath = dirname( __DIR__, 4 ) . '/resources/img';
+		$imageNames = [
+			'start-incoming.png',
+			'end-outgoing.png',
+			'task-incoming.png',
+			'task-outgoing.png'
+		];
+
+		foreach ( $imageNames as $name ) {
+			$images[ $name ] = "$imagesPath/$name";
+		}
+	}
+}
