4a7d87864cef5dadb5adf2d4012ed9982ae0cef4
Umherirrender <umherirrender_de.wp@web.de>
2025-11-03 02:23:36 +0100

    Use namespaced Title class
    Change-Id: I415c9368457aa76500b8ebde19cf94f5d727de15

--- maintenance/migrateDiagrams.php
+++ maintenance/migrateDiagrams.php
@@ -14,6 +14,7 @@ use MediaWiki\MediaWikiServices;
 use MediaWiki\Message\Message;
 use MediaWiki\Page\WikiPageFactory;
 use MediaWiki\Revision\SlotRecord;
+use MediaWiki\Title\Title;
 use SMW\DIProperty;
 use SMW\StoreFactory;
 
