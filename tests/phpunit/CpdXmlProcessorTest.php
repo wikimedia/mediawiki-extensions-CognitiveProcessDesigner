e473f866fbedc39a18b15995838fe89db1062ab5
Hamish <hamish.slater@hallowelt.com>
2026-04-23 14:35:57 +0700

    Fix CpdXmlProcessorTest
    MediaWikiTitleCodec -> TitleParser
    
    ERM47524
    
    [5.1.x]
    
    Change-Id: I17459f0264ecc36a8298731e956ce14ce2bbd7c9

--- tests/phpunit/CpdXmlProcessorTest.php
+++ tests/phpunit/CpdXmlProcessorTest.php
@@ -7,9 +7,9 @@ use CognitiveProcessDesigner\CpdElementFactory;
 use CognitiveProcessDesigner\Util\CpdXmlProcessor;
 use Exception;
 use MediaWiki\Config\Config;
-use MediaWiki\Title\MediaWikiTitleCodec;
 use MediaWiki\Title\NamespaceInfo;
 use MediaWiki\Title\Title;
+use MediaWiki\Title\TitleParser;
 use PHPUnit\Framework\TestCase;
 use TitleFactory;
 
@@ -45,8 +45,7 @@ class CpdXmlProcessorTest extends TestCase {
 		$titleFactoryMock = $this->createMock( TitleFactory::class );
 		$titleFactoryMock->method( 'newFromDBkey' )->willReturnCallback( static fn ( $dbkey ) => Title::newFromText( $dbkey ) );
 
-		$titleParserMock = $this->createMock( MediaWikiTitleCodec::class );
-		$titleParserMock->method( 'splitTitleString' )->willReturn( null );
+		$titleParserMock = $this->createMock( TitleParser::class );
 
 		$this->xmlProcessor = new CpdXmlProcessor(
 			$configMock, $nsInfoMock, new CpdElementFactory(
